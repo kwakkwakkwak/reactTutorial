f832d3d41f841d93f6a362bf3f476ba2ef938b0e
kwak <kwakkj112@naver.com>
2019-04-23 18:10:38 +0900

    react tutorial
    square 의 이벤트를 Board 로 이동

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import './index.css';
 
 // 게임판 안에 작은 상자
 class Square extends React.Component {
+
     render() {
         return (
-            <button className="square" onClick={()=> {alert(this.props.value)}}>
+            <button className="square" onClick={() => this.props.onClick()}>
                 {this.props.value}
             </button>
         );
@@ -15,11 +16,26 @@ class Square extends React.Component {
 }
 
 
-// 게임판에 해당 최상위
+// 게임판
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i}/>;
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
     }
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i) }/>;
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+
+    }
+
+
 
     render() {
         const status = 'Next player: X';
@@ -47,6 +63,7 @@ class Board extends React.Component {
     }
 }
 
+// 게임 자체
 class Game extends React.Component {
     render() {
         return (
